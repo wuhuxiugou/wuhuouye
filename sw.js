58c6ab2cd4b95999760b94e762c86a254b2bc146
wuhuxiugou <109937798+wuhuxiugou@users.noreply.github.com>
2025-07-25 20:47:33 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+// sw.js
+const CACHE_NAME = 'production-stats-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/icon-192x192.png',
+  '/icon-512x512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
